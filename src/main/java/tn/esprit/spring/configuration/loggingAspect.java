66c2539cae1652ed6b3921191d9ae081b69d74c9
Abir Halouani <abir.halouani7@gmail.com>
2022-02-18 16:31:06 +0100

    structure_projet

---
+++ src/main/java/tn/esprit/spring/configuration/loggingAspect.java
@@ -0,0 +1,41 @@
+package tn.esprit.spring.configuration;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+
+public class loggingAspect {
+private static final Logger l = LogManager.getLogger(loggingAspect.class);
+	
+	@Before("execution(* tn.esprit.spring.service.*.*(..))")
+	public void logMethodEntry(JoinPoint joinPoint) {
+		String name = joinPoint.getSignature().getName();
+		l.info("In method " + name + " : ");
+		}
+	
+	@Around("execution(* tn.esprit.spring.service.*.*(..))")
+	public void profile(ProceedingJoinPoint pjp) throws Throwable {
+	long start = System.currentTimeMillis();
+	pjp.proceed();
+	long elapsedTime = System.currentTimeMillis() - start;
+	l.info("Method execution time: " + elapsedTime + " milliseconds.");
+
+	}
+	
+	@AfterReturning("execution(* tn.esprit.spring.services.*.*(..))")
+	public void logMethodIn(JoinPoint joinPoint) {
+		String name = joinPoint.getSignature().getName();
+		l.info("In method " + name + " : ");
+		}
+
+}
