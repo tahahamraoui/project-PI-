9a02db8d672dc55b2a6b6ae8c52f8d675293880d
AbirHalouani <abir.halouani@esprit.tn>
2022-03-07 11:53:34 +0100

    crud+fonction avancée
    Co-Authored-By: marwaahadidane <47575717+marwaahadidane@users.noreply.github.com>

---
+++ src/main/java/tn/esprit/spring/services/ReponseService.java
@@ -0,0 +1,53 @@
+package tn.esprit.spring.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import tn.esprit.spring.entity.Reponse;
+import tn.esprit.spring.repository.ReponseRepository;
+
+@Service
+public class ReponseService implements IReponseService {
+
+		@Autowired
+		ReponseRepository ReponseRepo;
+		
+		@Override
+		public List<Reponse> retrieveAllReponse() {
+			List<Reponse> av = (List<Reponse>) ReponseRepo.findAll();
+			return av;
+		}
+
+		@Override
+		public Reponse addReponse(Reponse c) {
+			return ReponseRepo.save(c); 
+		}
+
+		@Override
+		public String deleteReponse(Integer id) {
+			if(ReponseRepo.findById(id).orElse(null) != null) {
+				ReponseRepo.deleteById(id);
+				return "Reponse removed successfully!";
+			}
+			else return "Reponse id not found!";
+		}
+
+		@Override
+		public Reponse updateidReponse(Reponse c) {
+			return ReponseRepo.save(c);
+		}
+
+		
+
+		@Override
+		public Reponse retrieveidReponse(int idR) {
+			return ReponseRepo.findById(idR).orElse(null);
+		}
+
+
+	
+}
