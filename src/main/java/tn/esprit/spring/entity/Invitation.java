936cee0f77bb96005a78d30feed53e6d2d46fc71
tahahamraoui <taha.hamraoui@esprit.tn>
2022-03-09 00:06:23 +0100

    integration wale

--- src/main/java/tn/esprit/spring/entity/Invitation.java
+++ src/main/java/tn/esprit/spring/entity/Invitation.java
@@ -3,6 +3,8 @@ package tn.esprit.spring.entity;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,11 +20,13 @@ import java.util.Set;
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @ToString
 @Table(name="Invitation")
 public class Invitation implements Serializable{
+	public Invitation() {
+		super();
+	}
+
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
@@ -31,11 +35,105 @@ public class Invitation implements Serializable{
 	@Column(name="dateinvitation")
 	private Date dateinvitation;
 	
+	@Column(name="objet")
+	private String objet;
+	
+	@Column(name="contenue")
+	private String contenue;
+	
 	@Column(name="statutenvoie")
 	private boolean statutenvoie;
 	
-	@Column(name="listeemails")
-	private String listeemails;
+	@Column(name="statusacceptation")
+	private boolean statusacceptation;
+	
+	@Column(name="email")
+	private String email;
+	
+	public Invitation( Date dateinvitation, String objet, String contenue, boolean statutenvoie,
+			boolean statusacceptation, String email) {
+		super();
+		this.dateinvitation = dateinvitation;
+		this.objet = objet;
+		this.contenue = contenue;
+		this.statutenvoie = statutenvoie;
+		this.statusacceptation = statusacceptation;
+		this.email = email;
+	}
+
+	public int getIdinvitation() {
+		return idinvitation;
+	}
+
+	public void setIdinvitation(int idinvitation) {
+		this.idinvitation = idinvitation;
+	}
+
+	public Date getDateinvitation() {
+		return dateinvitation;
+	}
+
+	public void setDateinvitation(Date dateinvitation) {
+		this.dateinvitation = dateinvitation;
+	}
+
+	public String getObjet() {
+		return objet;
+	}
+
+	public void setObjet(String objet) {
+		this.objet = objet;
+	}
+
+	public String getContenue() {
+		return contenue;
+	}
+
+	public void setContenue(String contenue) {
+		this.contenue = contenue;
+	}
+
+	public boolean isStatutenvoie() {
+		return statutenvoie;
+	}
+
+	public void setStatutenvoie(boolean statutenvoie) {
+		this.statutenvoie = statutenvoie;
+	}
+	
+	public boolean isStatusacceptation() {
+		return statusacceptation;
+	}
+
+	public void setStatusacceptation(boolean statusacceptation) {
+		this.statusacceptation = statusacceptation;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public Invitation(int idinvitation, Date dateinvitation, String objet, String contenue, boolean statutenvoie,
+			boolean statusacceptation, String email) {
+		super();
+		this.idinvitation = idinvitation;
+		this.dateinvitation = dateinvitation;
+		this.objet = objet;
+		this.contenue = contenue;
+		this.statutenvoie = statutenvoie;
+		this.statusacceptation = statusacceptation;
+		this.email = email;
+	}
+
+	
 
 
 
--- src/main/java/tn/esprit/spring/services/InvitationService.java
+++ src/main/java/tn/esprit/spring/services/InvitationService.java
@@ -1,16 +1,22 @@
 package tn.esprit.spring.services;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tn.esprit.spring.entity.Invitation;
 import tn.esprit.spring.repository.InvitationRepository;
+import tn.esprit.spring.services.SendEmailService;
 
 @Service
 public class InvitationService implements IInvitationService {
 	 @Autowired
 	 InvitationRepository myRepository ;
+	 
+	 @Autowired
+	 SendEmailService sendemailservice ;
 	
 	 @Override
 	 public List<Invitation> retrieveAllInvitations() {
@@ -19,6 +25,7 @@ public class InvitationService implements IInvitationService {
 	 }
 	 @Override
 	 public Invitation addInvitation(Invitation c){
+		 c.setStatutenvoie(false);
 		 return myRepository.save(c);
 	 }
 	 @Override
@@ -35,6 +42,42 @@ public class InvitationService implements IInvitationService {
 			Optional<Invitation> c = myRepository.findById(id);
 			Invitation c1 = c.get();
 			return c1;
+	}
+	 @Override
+	 public List<Integer> sendInvitations(List<String> to) {
+		 String[] toArray = new String [to.size()];
+		 List<Integer> ids = new ArrayList<Integer> ();
+		 int i = 0 ;
+		 for(String email : to ) {
+			 Date today = new Date();
+			 Invitation invitation = new Invitation(
+					 today,
+					 "Invitation",
+					 "veuillez Accepter l'invitation",
+					 true,
+					 false,
+					 email);
+			 invitation = myRepository.save(invitation);
+			 ids.add(invitation.getIdinvitation());
+			 toArray[i] = email;
+			 i++;
+		 }
+		 sendemailservice.sendEmail(toArray, "noreply@invitation.com", "Veuillez Accepter l'invitation","Invitation");
+		 return ids;
+	 }
+	 
+	@Override
+	public boolean acceptInvitation(Integer id) {
+		Invitation invitation = myRepository.findById(id).get();
+		invitation.setStatusacceptation(true);
+		myRepository.save(invitation);
+		return true;
+	}
+	
+	@Override
+	public boolean consulterAcceptation(Integer id) {
+		Invitation invitation = myRepository.findById(id).get();
+		return invitation.isStatusacceptation();
 	}
 	
 	
